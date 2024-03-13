770cd55d2052a585c701a3a701b747b98169a0bb
fernaldr <121467203+fernaldr@users.noreply.github.com>
2024-03-12 23:30:40 -0400

    moved index,intro, vampire-input form into /templates. moved styles, images into /static

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, send_from_directory
+import os
+print(os.getcwd())
 
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('intro')
+@app.route('/intro')
 def intro():
     return render_template('intro.html')
 
-@app.route('vampire-input-form')
-def intro():
-    return render_template('vampire-input-form')
+@app.route('/vampire-input-form')
+def inputform():
+    return render_template('vampire-input-form.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    return "Form submitted!"
 
 if __name__ == '__main__':
     app.run(debug=True) 
