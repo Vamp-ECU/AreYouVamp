e7624ec1958e109b57e4301bf4a6a873b7d75d29
fernaldr <121467203+fernaldr@users.noreply.github.com>
2024-03-12 22:50:03 -0400

    aaa

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('')
 def index():
     return render_template('index.html')
 
-@app.route('/intro')
+@app.route('intro')
 def intro():
     return render_template('intro.html')
 
-# Route to serve static files
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
+@app.route('vampire-input-form')
+def intro():
+    return render_template('vampire-input-form')
 
 if __name__ == '__main__':
     app.run(debug=True) 
